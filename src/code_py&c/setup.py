193903dbed1ba770f11ee4d58981388952f049b1
ValletRomain <61109605+ValletRomain@users.noreply.github.com>
2020-10-16 11:20:45 +0200

    Delete setup.py

--- src/code_py&c/setup.py
+++
@@ -1,9 +0,0 @@
-from distutils.core import setup, Extension
-
-module1 = Extension('godunov',
-                    sources = ['godunov.c'])
-
-setup (name = 'Godunov',
-       version = '1.0',
-       description = 'This is a demo godunov package',
-       ext_modules = [module1])
