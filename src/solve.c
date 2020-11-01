2df6816e9ae88cedae6f149e0b49d592457e5c44
ValletRomain <romainvallet@live.fr>
2020-11-01 19:27:48 +0100

    Add of new option : - g resolve godunov - r resolve rusanov

--- src/solve.c
+++ src/solve.c
@@ -1,5 +1,5 @@
 /* Application godunov : resolve the schema of godunov
-usage : godunov -v path_input path_output
+usage : godunov {g|r|gr} [v] path_input path_output
 */
 
 #include <stdlib.h>
@@ -13,28 +13,39 @@ usage : godunov -v path_input path_output
 
 int main(int argc, char * argv[]){
 
-    int c, errflag=0, aflag=0;
+    int c, errflag=0, aflag=0, gflag=0, rflag=0;
     extern char *optarg;
     extern int optind;
 
-    while ( (c=getopt(argc, argv, "a"))!=-1 ){
+    while ( (c=getopt(argc, argv, "agr"))!=-1 ){
         switch (c)
         {
         case 'a':
             aflag++;
             break;
         
+        case 'g':
+            gflag++;
+            break;
+
+        case 'r':
+            rflag++;
+            break;
+
         default:
             errflag++;
             break;
         }
     }
 
+    if ((gflag==0) && (rflag==0))
+        errflag++;
+
     if ((argc-optind) != 2)
         errflag++;
     
     if (errflag)
-        raler(0, "usage : godunov [a] path_input path_output");
+        raler(0, "usage : godunov {g|r|gr} [a] path_input path_output");
 
     char * path_input = malloc(CHEMIN_MAX);
     char * path_output = malloc(CHEMIN_MAX);
