06724ec6f324210aea54b0afa864de2ebc6a2c90
ValletRomain <romainvallet@live.fr>
2020-11-01 19:23:08 +0100

    Rename : godunov -> solve godunov_error -> compute_error

--- use_me.bash
+++ use_me.bash
@@ -10,9 +10,9 @@ cd ..
 # output is the directory wich contains the result
 mkdir output
 
-./build/godunov input/solve_transport_1d_1 output/  # resolve of equation of transport
-./build/godunov input/solve_burgers_1d_1 output/    # resolve of equation of burgers
-./build/godunov_error input/error_transport_1d_1 output/    # compute of error and time for equation of transport
-./build/godunov_error input/error_burgers_1d_1 output/      # compute of error and time for equation if burgers
+./build/solve input/solve_transport_1d_1 output/  # resolve of equation of transport
+./build/solve input/solve_burgers_1d_1 output/    # resolve of equation of burgers
+./build/compute_error input/error_transport_1d_1 output/    # compute of error and time for equation of transport
+./build/compute_error input/error_burgers_1d_1 output/      # compute of error and time for equation if burgers
 
 
