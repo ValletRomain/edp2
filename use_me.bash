61a574c139eb69f66bb8a4e18a769d7bbdfd9ca8
ValletRomain <romainvallet@live.fr>
2020-12-31 18:16:58 +0100

    Update

--- use_me.bash
+++ use_me.bash
@@ -18,21 +18,25 @@ echo
 # output is the directory wich contains the result
 echo Creation de output
 mkdir output
+cd output
+mkdir solve
+mkdir error
+cd ..
 echo
 
 echo Resolution de l equation de transport
-./build/solve -grme input/solve_transport_1d/solve_transport_1d_1 output/  # resolve of equation of transport with Godunov, Rusanov and MUSCL method
+./build/solve -grme input/solve/transport/transport_1 output/solve/  # resolve of equation of transport with Godunov, Rusanov and MUSCL method
 echo
 
 echo Resolution de l equation de burgers
-./build/solve -grme input/solve_burgers_1d/solve_burgers_1d_1 output/    # resolve of equation of burgers with Godunov, Rusanov and MUSCL method
+./build/solve -grme input/solve/burgers/burgers_1 output/solve/    # resolve of equation of burgers with Godunov, Rusanov and MUSCL method
 echo
 
 echo Calcul des erreurs et du temps pour l equation de transport
-./build/compute_error -grm input/error_transport_1d/error_transport_1d_1 output/    # compute of error and time for equation of transport with Godunov, Rusanov and MUSCL method
+./build/compute_error -grm input/error/transport/transport_1 output/error/    # compute of error and time for equation of transport with Godunov, Rusanov and MUSCL method
 echo
 
 echo Calcul des erreurs et du temps pour l equation de burgers
-./build/compute_error -grm input/error_burgers_1d/error_burgers_1d_1 output/      # compute of error and time for equation if burgers with Godunov, Rusanov and MUSCL method
+./build/compute_error -grm input/error/transport/transport_1 output/error/     # compute of error and time for equation if burgers with Godunov, Rusanov and MUSCL method
 echo
 
